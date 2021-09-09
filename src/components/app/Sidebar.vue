acbf2eb1e08e59faaee71ed96dd86f0815ff7ccc
BurdenyukN <BurdenyukN@qmobile.ru>
2021-09-09 12:40:01 +0300

    2

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,41 @@
+<template>
+  <span>Сообщение: {{ modelValue }}</span>
+  <ul class="sidenav app-sidenav" :class="{open: modelValue}">
+    <template
+      v-for="link in links"
+      :key="link.url"
+      >
+      <li v-if="Object.is(link.url, this.$route.path)" class="active">
+        <router-link
+          :to="link.url"
+          class="waves-effect waves-orange pointer"
+        >
+          {{ link.title }}
+        </router-link>
+      </li>
+      <li v-else>
+        <router-link
+          :to="link.url"
+          class="waves-effect waves-orange pointer"
+        >
+          {{ link.title }}
+        </router-link>
+      </li>
+    </template>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['modelValue'],
+  data: () => ({
+    links: [
+      {title: 'Счет', url: '/'},
+      {title: 'История', url: '/history'},
+      {title: 'Планирование', url: '/planning'},
+      {title: 'Новая запись', url: '/record'},
+      {title: 'Категории', url: '/categories'},
+    ]
+  })
+}
+</script>
